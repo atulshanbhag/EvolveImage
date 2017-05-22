5de6061572ea249910c84415141be2a3b80b6b97
Atul Shanbhag <shanbhag1996@gmail.com>
2017-05-22 18:09:18 +0530

    tests

--- evolve_ga.py
+++ evolve_ga.py
@@ -13,16 +13,16 @@ from PIL import Image, ImageDraw
 
 
 # Target image file and location.
-TARGET_IMAGE_NAME = 'monalisa.png'
-TARGET_LOCATION = os.getcwd() + '\{0}'.format(TARGET_IMAGE_NAME)
+TARGET_IMAGE_NAME = "monalisa.png"
+TARGET_LOCATION = os.path.join(os.getcwd(), TARGET_IMAGE_NAME)
 
 
 # Load target image file. Error if doesn't exist.
 try:
     target_image = Image.open(TARGET_LOCATION)
 except IOError:
-    print('Target image {0} not found. Must be placed as {0}'.format(
-        TARGET_NAME, TARGET_LOCATION))
+    print("Target image {0} not found. Must be placed as {0}".format(
+        TARGET_IMAGE_NAME, TARGET_LOCATION))
     sys.exit()
 
 
@@ -51,13 +51,13 @@ class Point(object):
 
     # Add 2 points
     def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
+        return Point(self._x + other.x, self._y + other.y)
 
     def __eq__(self, other):
-        return (self.x == other.x) and (self.y == other.y)
+        return (self._x == other.x) and (self._y == other.y)
 
     def __repr__(self):
-        return 'Point({0}, {1})'.format(self.x, self.y)
+        return "Point({0}, {1})".format(self._x, self._y)
 
 
 class Color(object):
@@ -89,11 +89,8 @@ class Color(object):
         return self._alpha
 
     def __repr__(self):
-        return 'Color({0}, {1}, {2}, {3})'.format(
-            self.r, self.g, self.b, self.alpha)
-
-
-GENE_PARAMS = ['diameter', 'pos', 'color']
+        return "Color({0}, {1}, {2}, {3})".format(
+            self._r, self._g, self._b, self._alpha)
 
 
 class Gene(object):
@@ -129,49 +126,61 @@ class Gene(object):
         return self._color
 
     def __repr__(self):
-        return 'Gene( Diameter({0}), {1}, {2} )'.format(
-            self.diameter, self.pos, self.color)
+        return "Gene( Diameter({0}), {1}, {2} )".format(
+            self._diameter, self._pos, self._color)
+
+    # Mutation type parameters.
+    PARAMS = ["diameter", "pos", "color"]
 
     def mutate(self):
         # Randomly choose the amount of mutation to be done.
         mutation_size = max(1, int(round(random.gauss(15, 4)))) // 100
 
-        # Randomly choose the mutation type from GENE_PARAMS.
+        # Randomly choose the mutation type from PARAMS.
         # Options --> diameter, pos, color
-        mutation_type = random.choice(GENE_PARAMS)
+        mutation_type = random.choice(Gene.PARAMS)
 
-        if mutation_type == 'diameter':
+        if mutation_type == "diameter":
             self._diameter = max(1, random.randint(int(
-                self._diameter * (1 - mutation_size)), int(self.diameter * (1 + mutation_size))))
+                self._diameter * (1 - mutation_size)), int(
+                self.diameter * (1 + mutation_size))))
 
-        elif mutation_type == 'pos':
+        elif mutation_type == "pos":
             x = max(0, random.randint(
-                int(self.pos.x * (1 - mutation_size)), int(self.pos.x * (1 + mutation_size))))
+                int(self.pos.x * (1 - mutation_size)), int(
+                    self.pos.x * (1 + mutation_size))))
             y = max(0, random.randint(
-                int(self.pos.y * (1 - mutation_size)), int(self.pos.y * (1 + mutation_size))))
+                int(self.pos.y * (1 - mutation_size)), int(
+                    self.pos.y * (1 + mutation_size))))
             self._pos = Point(min(x, TARGET_IMAGE_WIDTH), min(
                 y, TARGET_IMAGE_HEIGHT))
 
         # mutation_type == color
         else:
-            r = min(max(0, random.randint(int(self.color.r * (1 - mutation_size)),
-                                          int(self.color.r * (1 + mutation_size)))), 255)
-            g = min(max(0, random.randint(int(self.color.g * (1 - mutation_size)),
-                                          int(self.color.g * (1 + mutation_size)))), 255)
-            b = min(max(0, random.randint(int(self.color.b * (1 - mutation_size)),
-                                          int(self.color.b * (1 + mutation_size)))), 255)
-            alpha = min(max(0, random.randint(int(self.color.alpha * (1 - mutation_size)),
-                                              int(self.color.alpha * (1 + mutation_size)))), 255)
+            r = min(max(0, random.randint(
+                int(self.color.r * (1 - mutation_size)),
+                int(self.color.r * (1 + mutation_size)))), 255)
+            g = min(max(0, random.randint(
+                int(self.color.g * (1 - mutation_size)),
+                int(self.color.g * (1 + mutation_size)))), 255)
+            b = min(max(0, random.randint(
+                int(self.color.b * (1 - mutation_size)),
+                int(self.color.b * (1 + mutation_size)))), 255)
+            alpha = min(max(0, random.randint(
+                int(self.color.alpha * (1 - mutation_size)),
+                int(self.color.alpha * (1 + mutation_size)))), 255)
+
             self._color = Color(r, g, b, alpha)
 
     # Save the gene in case of program interrupt.
     def save(self):
-        """ Save the gene object in case 
+        """Save the gene object in case 
         of the program interrupt.
         """
-        s = {'diameter': self._diameter,
-             'pos': self._pos,
-             'color': self._color}
+
+        s = {"diameter": self._diameter,
+             "pos": self._pos,
+             "color": self._color}
         return s
 
     # Load the saved gene.
@@ -179,9 +188,10 @@ class Gene(object):
         """ Load the gene object from 
         save parameter
         """
-        self._diameter = s['diameter']
-        self._pos = s['pos']
-        self._color = s['color']
+
+        self._diameter = s["diameter"]
+        self._pos = s["pos"]
+        self._color = s["color"]
 
 
 def fitness(img1, img2):
@@ -200,12 +210,12 @@ def fitness(img1, img2):
     im2 = np.array(img2, dtype=np.int16)
     return (np.abs(im1 - im2).mean() / 255 * 100)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # test gene
     g = Gene()
 
     # test save working fine
-    print(g.save())
+    print("Initial save", g.save())
 
     # test blank image
     img = Image.new("RGB", TARGET_IMAGE_SIZE, (255, 255, 255))
@@ -216,15 +226,13 @@ if __name__ == '__main__':
     print("multiprocessing count =", multiprocessing.cpu_count())
 
     # test load and save feature using jsonpickle
-    f = open('logs.txt', 'w')
-    f.write(jsonpickle.encode(g.save()))
-    f.close()
+    with open("logs.txt", 'w+') as f:
+        f.write(jsonpickle.encode(g.save()))
 
-    f = open('logs.txt', 'r')
-    d = jsonpickle.decode(f.read())
-    f.close()
+        f.seek(0)
+        d = jsonpickle.decode(f.read())
 
-    print(d)
+    print("Final save", d)
     g.load(d)
     print(g)
 
